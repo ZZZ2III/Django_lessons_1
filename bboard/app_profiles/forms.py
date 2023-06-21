8f083c82794b56451a5557ce3e08bfc9ec418863
ZZZ2III <107794582+ZZZ2III@users.noreply.github.com>
2023-06-21 18:02:32 +0300

    21.06.2023

--- bboard/app_profiles/forms.py
+++ bboard/app_profiles/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+import datetime
+from django.core.exceptions import ValidationError
 
 class UserForm(forms.Form):
     username = forms.CharField()
@@ -9,8 +11,27 @@ class UserForm(forms.Form):
     email = forms.EmailField()
     birthday = forms.DateField()
 
+    def clean_birthday(self):
+        data = self.cleaned_data['birthday']
+        #print(data,type(data))
+        today = datetime.date.today()
+        year_delta = (today-data) / 365
+        if year_delta < 18 or year_delta<0:
+            raise ValidationError('Регистрироваться на сайте могут только лица старше 18 лет')
+        return data
+
+    def clean(self):
+        cleaned_data = super(UserForm,self).clean()
+        first_name = cleaned_data.get['firstname']
+        last_name = cleaned_data.get['last_name']
+        if first_name == 'Иван' and last_name == 'Иванов':
+            msg = 'Запрещено регистрироваться Иванам Ивановым'
+            self.add_error('firstname',msg)
+            self.add_error('last_name',msg)
+            #raise ValidationError()
+
 
 class AdvertisementForm(forms.Form):
-    title = forms.CharField()
-    description = forms.Textarea()
-    price = forms.IntegerField()
+    title = forms.CharField(max_length=20,min_length=3)
+    description = forms.CharField(max_length=200,min_length=10)
+    price = forms.IntegerField(min_value=10,max_value=100000)
--- bboard/app_profiles/models.py
+++ bboard/app_profiles/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=20,verbose_name='Псевдоним')
-    password = models.CharField(max_length=20,verbose_name='Пароль')
-    firstname = models.CharField(max_length=20,verbose_name='Имя')
-    second_name = models.CharField(max_length=20,verbose_name='Отчество')
-    last_name = models.CharField(max_length=20,verbose_name='Фамилия')
+    username = models.CharField(max_length=20, verbose_name='Псевдоним')
+    password = models.CharField(max_length=20, verbose_name='Пароль')
+    firstname = models.CharField(max_length=20, verbose_name='Имя')
+    second_name = models.CharField(max_length=20, verbose_name='Отчество')
+    last_name = models.CharField(max_length=20, verbose_name='Фамилия')
     email = models.EmailField(verbose_name='Почта')
     birthday = models.DateField(verbose_name='День рождения')
 
+
 class Advertisement(models.Model):
     title = models.CharField(max_length=20)
-    description = models.TextField()
+    description = models.CharField(max_length=20)
     price = models.IntegerField()
